9f1a7f74be4dbf54ab041c082e0f208b4c34a541
Logan <logan.hitchcock54@gmail.com>
2022-06-30 00:32:47 -0600

    working on card class

---
+++ card.py
@@ -0,0 +1,44 @@
+import random
+class Card:
+
+    def __init__(self):
+        self.value = 0 
+
+
+    def card(self):
+        self.value = random.randint(1, 13)
+
+
+#Above and below are two seperate ideas 
+
+
+    def cards():
+        card_deck = []
+        for i in len(0,13):
+            card_deck.append(i)
+        return card_deck
+
+    def new_card():
+        card_number = ''
+
+        return card_number
+        pass 
+
+    def user_guess(card_deck):
+        user_input = ('Higher or Lower? (h/l)')
+        #REMINDER 
+        #the director will pull a new number, make a new class that will compare the old number with the new number 
+        # The director directs through the class 
+
+    
+    def update():
+        pass
+
+
+
+
+
+
+
+
+
---
+++ director.py
@@ -0,0 +1,11 @@
+
+class Director:
+
+    def director():
+        score = 300
+
+        while score > 0:
+            # run through the program:
+            pass 
+
+        print('Thanks for playing')
