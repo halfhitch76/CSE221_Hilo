5fa5d066753492594b732a454b2c68596c4af979
Logan <logan.hitchcock54@gmail.com>
2022-06-30 00:52:06 -0600

    card and main arefinished. Just need to work on director

--- card.py
+++ card.py
@@ -12,27 +12,6 @@ class Card:
 #Above and below are two seperate ideas 
 
 
-    def cards():
-        card_deck = []
-        for i in len(0,13):
-            card_deck.append(i)
-        return card_deck
-
-    def new_card():
-        card_number = ''
-
-        return card_number
-        pass 
-
-    def user_guess(card_deck):
-        user_input = ('Higher or Lower? (h/l)')
-        #REMINDER 
-        #the director will pull a new number, make a new class that will compare the old number with the new number 
-        # The director directs through the class 
-
-    
-    def update():
-        pass
 
 
 
--- director.py
+++ director.py
@@ -1,11 +1,31 @@
+from card import Card
 
 class Director:
 
-    def director():
-        score = 300
+    def __init__(self):
+        
+        self.dice = []
+        self.is_playing = True
+        self.score = 300
+        self.total_score = 0
 
-        while score > 0:
-            # run through the program:
-            pass 
 
-        print('Thanks for playing')
+
+    def start_game(self):
+
+        while self.is_playing():
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        pass 
+
+
+    def do_updates(self):
+        pass
+
+    def do_outputs(self):
+        pass
+
+    
